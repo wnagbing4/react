6c304d944cf3f21377da131c7e8e15d8fec391d5
3396515303@qq.com <12267007+wangbing56@user.noreply.gitee.com>
2024-07-11 19:53:17 +0800

    item悬浮效果

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
 import "./index.css"
-export default class Item extends Component {
-    render() {
-        const {name,id,done} =this.props
-        return (
 
-                <li>
-                    <label>
-                        <input type='checkbox' defaultChecked={done}/>
-                        <span>{name}</span>
-                    </label>
-                    <button className='btn btn-danger' style={{display: 'none'}}>删除</button>
-                </li>
-        );
+export default class Item extends Component {
+    state = {mouse: false}
+    handleMouse = (flag) => {
+        return () => {
+            this.setState({mouse: flag})
+        }
+    }
+
+    render() {
+        const {name, id, done} = this.props
+        const {mouse} = this.state
+        return (
+            <li style={{backgroundColor: mouse ? '#ddd' : 'white'}} onMouseLeave={this.handleMouse(false)}
+                onMouseEnter={this.handleMouse(true)}>
+                <label>
+                    <input type='checkbox' defaultChecked={done}/>
+                    <span>{name}</span>
+                </label>
+                <button className='btn btn-danger' style={{display: mouse ? 'block' : 'none'}}>删除</button>
+            </li>);
     }
 }
 
