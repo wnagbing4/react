bded228c997acc95ac90a484f365927fdbfa631e
3396515303@qq.com <12267007+wangbing56@user.noreply.gitee.com>
2024-07-11 08:42:25 +0800

    todoList静态页面

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import "./index.css"
+export default class Header extends Component {
+    handleKeyUp=(event)=>{
+        const {keyCode,target} =event
+        if(keyCode!==13) return
+        console.log(target.value)
+        this.props.addTodo(target.value)
+    }
+    render() {
+        return (
+            <div className='todo-header'>
+                <input onKeyUp={this.handleKeyUp} type='text' placeholder='请输入你的任务名称,按回车键确认'/>
+            </div>
+        );
+    }
+}
+
